1c862c07140a0cefc67811b17e6adbd6d33c5021
Greg Morton <gregorymorton96@gmail.com>
2022-02-23 10:13:47 -0500

    load_details

--- project/expenses/management/commands/load_summary.py
+++ project/expenses/management/commands/load_summary.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print "Loading CSV"
+        print ("Loading CSV")
         csv_path = "./summary.csv"
         csv_file = open(csv_path, 'r')
         csv_reader = csv.DictReader(csv_file)
