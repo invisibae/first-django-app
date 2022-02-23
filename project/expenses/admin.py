ed8947bc73d7ce2550faa0794a1254eb7dd01ece
Greg Morton <gregorymorton96@gmail.com>
2022-02-23 11:08:11 -0500

    filter function

--- project/expenses/admin.py
+++ project/expenses/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from expenses.models import Summary
-# Register your models here.
-admin.site.register(Summary)
+# Register your models here
 
 class SummaryAdmin(admin.ModelAdmin): # Creates a 'SummaryAdmin' class that is tied to 'summary'
     list_display = ("office", "program", "category", "amount") #choose what variables to display
 
 admin.site.register(Summary, SummaryAdmin)
-    
