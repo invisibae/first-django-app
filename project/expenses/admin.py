c7348e832bbf30aa84a4497e90098aa192223961
Greg Morton <gregorymorton96@gmail.com>
2022-02-23 11:08:57 -0500

    filter function

--- project/expenses/admin.py
+++ project/expenses/admin.py
@@ -4,5 +4,6 @@ from expenses.models import Summary
 
 class SummaryAdmin(admin.ModelAdmin): # Creates a 'SummaryAdmin' class that is tied to 'summary'
     list_display = ("office", "program", "category", "amount") #choose what variables to display
+    list_filter = ("category", "program") # adds a filter function
 
 admin.site.register(Summary, SummaryAdmin)
