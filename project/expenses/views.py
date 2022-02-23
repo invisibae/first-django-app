128c58c919f69bc96fa43c22ef3430b51c6e0469
Greg Morton <gregorymorton96@gmail.com>
2022-02-23 11:47:01 -0500

    f str

--- project/expenses/views.py
+++ project/expenses/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from expenses.models import Summary, Detail
 
 def index(request): # index takes 'request' as an argument #describes the request being passed # request object contains MANY different types of attributes
-  return HttpResponse("Hello, world. You're at the expenses index.")
+  total = Summary.objects.count()
+
+  return HttpResponse(f"Hello, world. You're at the expenses index and we have some {total} of summery objects.")
 
 # Create your views here.
