006f826e490f0d7a598e468583e0fcaae253b1ff
Greg Morton <gregorymorton96@gmail.com>
2022-02-23 11:19:51 -0500

    view "hello world"

--- project/expenses/views.py
+++ project/expenses/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request): # index takes 'request' as an argument #describes the request being passed # request object contains MANY different types of attributes
+  return HttpResponse("Hello, world. You're at the expenses index.")
 
 # Create your views here.
