b36b626fba51c69bf85ff49b5c0fe688a28025d2
Greg Morton <gregorymorton96@gmail.com>
2022-02-23 11:05:24 -0500

    create SummaryAdmin class

--- project/expenses/admin.py
+++ project/expenses/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from expenses.models import Summary
 # Register your models here.
 admin.site.register(Summary)
+
+class SummaryAdmin(admin.ModelAdmin): # Creates a 'SummaryAdmin' class that is tied to 'summary'
+    list_display = ("office", "program", "category", "amount") #choose what variables to display
+
+admin.site.register(Summary, SummaryAdmin)
+    
