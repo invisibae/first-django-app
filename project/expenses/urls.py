5f15c46db29c71af0f82dd7110a8f08c58042e13
Greg Morton <gregorymorton96@gmail.com>
2022-02-23 11:38:50 -0500

    saved urls

--- project/expenses/urls.py
+++ project/expenses/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 
 from . import views
 
-
-from django.contrib import admin
-from django.urls import include, path
-
 urlpatterns = [
-  path('expenses/', include('expenses.urls')),
-  path('admin/', admin.site.urls),
+  path('', views.index, name='index'),
 ]
