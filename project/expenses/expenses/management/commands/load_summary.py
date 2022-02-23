e519675e64aa3820c818f53c7ce3b748e632b1d4
Greg Morton <gregorymorton96@gmail.com>
2022-02-23 09:58:47 -0500

    test

--- project/expenses/expenses/management/commands/load_summary.py
+++ project/expenses/expenses/management/commands/load_summary.py
@@ -1,6 +1,11 @@
+import csv
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print("Loading CSV")
+        csv_path = "./summary.csv"
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            print(row)
