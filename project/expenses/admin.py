0680ea9ac72bf24bd46e76e166504faea398c7e0
Greg Morton <gregorymorton96@gmail.com>
2022-02-23 11:15:29 -0500

    search function and detail

--- project/expenses/admin.py
+++ project/expenses/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from expenses.models import Summary
+from expenses.models import Summary, Detail
 # Register your models here
 
 class SummaryAdmin(admin.ModelAdmin): # Creates a 'SummaryAdmin' class that is tied to 'summary'
     list_display = ("office", "program", "category", "amount") #choose what variables to display
     list_filter = ("category", "program") # adds a filter function
+    search_fields = ("program",) # add search field # comma creates 'tuple' (an immutable list)
+
+
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ("office", "program", "category", "payee", "purpose", "amount")
+    list_filter = ("category", "program", "purpose")
+    search_fields = ("program", "payee")
 
 admin.site.register(Summary, SummaryAdmin)
+admin.site.register(Detail, DetailAdmin)
